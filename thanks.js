1eab3f2eaaac3fef2cc901be7f56810c4a6c33c0
sho-tanabe <93310029+sho-tanabe@users.noreply.github.com>
2021-11-06 12:14:01 +0900

    Update thanks.js

--- thanks.js
+++ thanks.js
@@ -1,1 +1,7 @@
-aa
+$(function () {
+    $('form').submit(function () {
+        closeWin();
+//        setTimeout(closeWin, 3000);/////これだとボタンを押した3秒後にとじる。実現したいのとは違った・・・
+        return false;
+    });
+});

b506958774290ad895a9ae8b5dbddccb413e5758
sho-tanabe <93310029+sho-tanabe@users.noreply.github.com>
2021-11-07 02:35:22 +0900

    Update index.js

--- index.js
+++ index.js
@@ -19,6 +19,24 @@ $(function () {
     });
 
     
+    
+            
+
+    
+    ////選択肢に応じて動的に項目を表示非表示させるファンクションのテスト開始
+    
+    $('form').document.getElementById("ex_age_b").addEventListener("change", function(){
+        var age_elem = document.getElementById("ex_age_b");
+        var s_value = age_elem.options[age_elem.selectedIndex].value;
+        if(s_value == "answer1"){$("#ex_box_div_b").hide("normal");}else{$("#ex_box_div_b").show("normal");}        
+    }, false);
+    ////選択肢に応じて動的に項目を表示非表示させるファンクションのテスト開始
+    
+    
+    
+    
+    
+    
     //ブラウザバリデーション　→ 原因不明だがトーク投稿に至らないため、失敗。いったんコメントアウト中。ここが悪いのではなくhtml側の問題かもしれない。。。調査中。
 
 //    $(function () {
@@ -40,16 +58,7 @@ $(function () {
     $('form').submit(function () {
 
 
-        
 
-        ////選択肢に応じて動的に項目を表示非表示させるファンクションのテスト開始
-        $('document.getElementById("ex_age_b")').addEventListener("change", function(){
-        
-            var age_elem = document.getElementById("ex_age_b");
-            var s_value = age_elem.options[age_elem.selectedIndex].value;	
-            if(s_value == "answer1"){$("#ex_box_div_b").hide("normal");}else{$("#ex_box_div_b").show("normal");}
-        }, false);
-        ////選択肢に応じて動的に項目を表示非表示させるファンクションのテスト開始
 
         
         
